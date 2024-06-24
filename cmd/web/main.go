74e591d07ed0f4fee2be51299dc636f64cf0756b
Saeem <saimmahmood61@gmail.com>
2024-06-24 16:22:26 +0500

    send email to users whenever there is a booking

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -31,6 +31,10 @@ func main() {
 		log.Fatal(err)
 	}
 	defer db.SQL.Close()
+	defer close(app.MailChan)
+
+	fmt.Println("Starting mail listener!")
+	listenForMail()
 
 	fmt.Printf("Starting application on port %s \n", portNumber)
 
@@ -49,6 +53,9 @@ func run() (*driver.DB, error) {
 	gob.Register(models.User{})
 	gob.Register(models.Room{})
 	gob.Register(models.Restriction{})
+
+	mailChan := make(chan models.MailData)
+	app.MailChan = mailChan
 
 	// change this to true when in production
 	app.InProduction = false
